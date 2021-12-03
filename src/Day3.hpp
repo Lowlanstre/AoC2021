3ee8bbcabc2304f15b1095ca27bc410c2faeff54
Megh <41010358+Lowlanstre@users.noreply.github.com>
2021-12-03 16:02:22 +0100

    Day 3 - Solution and Input

---
+++ src/Day3.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+class Day3 {
+
+public:
+
+    static void Solve();
+
+    static std::pair<int,int> binToDec(std::vector<int> inp);
+};
