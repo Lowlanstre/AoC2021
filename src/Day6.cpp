c986b47dc58ae5192f2e66ce2b4a3f2fe23442c0
Megh <41010358+Lowlanstre@users.noreply.github.com>
2021-12-06 18:35:26 +0100

    Day 6 - Input and Solution

---
+++ src/Day6.cpp
@@ -0,0 +1,56 @@
+#include "Day6.hpp"
+
+void Day6::Solve() {
+
+    typedef std::vector<unsigned  long long> VecULL;
+
+    const int days = 256;
+
+    int partOne = 0;
+    unsigned long long partTwo = 0;
+
+    std::ifstream inputStream;
+    inputStream.open("..\\input\\day6.txt");
+
+    VecULL fish(9,0);
+    VecULL bufferFish(9,0);
+
+    while (!inputStream.eof()) {
+
+        int number;
+        char garbage;
+
+        inputStream >> number >> garbage;
+
+        ++fish.at(number);
+
+    }
+
+    for(int i = 0; i < days; ++i) {
+
+        for (int j = 0; j < fish.size(); ++j) {
+
+            bufferFish.at(wrap(j)) += fish.at(j);
+
+        }
+
+        bufferFish.at(8) = fish.at(0);
+
+        fish = bufferFish;
+
+        for (auto& val: bufferFish)
+            val = 0;
+    }
+
+    for(auto x : fish)
+        partTwo += x;
+
+    std::cout << "The answer to part 1 is: " << partOne << "\n";
+    std::cout << "The answer to part 2 is: " << partTwo << "\n";
+}
+
+int Day6::wrap(int n) {
+    if (!n)
+        return 6;
+    return n-1;
+}
---
+++ src/Day6.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <numeric>
+
+class Day6 {
+
+public:
+
+    static void Solve();
+
+    static int wrap(int n);
+};
