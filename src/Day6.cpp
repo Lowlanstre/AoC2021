647a24e028a653377f9a92ed9eafe4a756a1ce99
Megh <41010358+Lowlanstre@users.noreply.github.com>
2021-12-06 18:39:18 +0100

    Day 6 - Fixed missing part one ;)

--- src/Day6.cpp
+++ src/Day6.cpp
@@ -4,9 +4,10 @@ void Day6::Solve() {
 
     typedef std::vector<unsigned  long long> VecULL;
 
-    const int days = 256;
+    const int days1 = 80;
+    const int days2 = 256;
 
-    int partOne = 0;
+    unsigned long long partOne = 0;
     unsigned long long partTwo = 0;
 
     std::ifstream inputStream;
@@ -26,7 +27,7 @@ void Day6::Solve() {
 
     }
 
-    for(int i = 0; i < days; ++i) {
+    for(int i = 0; i < days2; ++i) {
 
         for (int j = 0; j < fish.size(); ++j) {
 
@@ -40,6 +41,11 @@ void Day6::Solve() {
 
         for (auto& val: bufferFish)
             val = 0;
+
+        if(i == days1-1) {
+            for(auto x : fish)
+                partOne += x;
+        }
     }
 
     for(auto x : fish)
